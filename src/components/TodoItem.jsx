9775c9e196a170ec5adc77aa317153befafa9431
daulat <daulatsingh7878@gmail.com>
2026-05-23 23:01:46 +0530

    bug fixes

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,50 +2,65 @@ import { FaEdit } from "react-icons/fa";
 import { AiFillDelete } from "react-icons/ai";
 
 function TodoItem({ todo, onToggle, onDelete, onEdit }) {
+
     return (
         <div className="flex justify-between items-center bg-white rounded-xl px-3 py-2 my-2 shadow-sm">
 
-        {/* LEFT SIDE */}
-        <div className="flex gap-3 items-center flex-1">
-            
-            {/* Checkbox */}
-            <input
-            type="checkbox"
-            checked={todo.isCompleted}
-            onChange={() => onToggle(todo.id)}
-            className="w-4 h-4 accent-[#B11226] cursor-pointer"
-            />
+            {/* LEFT SIDE */}
+            <div className="flex gap-3 items-center flex-1">
 
-            {/* Text */}
-            <span
-            className={`text-sm ${
-                todo.isCompleted ? "line-through text-gray-400" : "text-gray-800"
-            }`}
-            >
-            {todo.todo}
-            </span>
-        </div>
+                {/* CHECKBOX */}
+                <input
+                    type="checkbox"
+                    checked={todo.isCompleted}
+                    onChange={() => onToggle(todo.id)}
+                    className="w-4 h-4 accent-[#B11226] cursor-pointer"
+                />
 
-        {/* RIGHT SIDE BUTTONS */}
-        <div className="flex gap-2 ml-2">
+                {/* TODO TEXT */}
+                <span
+                    className={`text-sm ${
+                        todo.isCompleted
+                            ? "line-through text-gray-400"
+                            : "text-gray-800"
+                    }`}
+                >
+                    {todo.todo}
+                </span>
+            </div>
 
-            {/* EDIT */}
-            <button
-            onClick={() => onEdit(todo.id)}
-            className="bg-[#B11226] hover:bg-[#8f0e1f] p-2 rounded-lg text-white transition duration-200"
-            >
-            <FaEdit size={14} />
-            </button>
+            {/* BUTTONS */}
+            <div className="flex gap-2 ml-2">
 
-            {/* DELETE */}
-            <button
-            onClick={() => onDelete(todo.id)}
-            className="bg-[#B11226] hover:bg-[#8f0e1f] p-2 rounded-lg text-white transition duration-200"
-            >
-            <AiFillDelete size={14} />
-            </button>
+                {/* EDIT */}
+                <button
+                    onClick={() => onEdit(todo.id)}
+                    className="
+                        bg-[#B11226]
+                        hover:bg-[#8f0e1f]
+                        p-2 rounded-lg
+                        text-white
+                        transition duration-200
+                    "
+                >
+                    <FaEdit size={14} />
+                </button>
 
-        </div>
+                {/* DELETE */}
+                <button
+                    onClick={() => onDelete(todo.id)}
+                    className="
+                        bg-[#B11226]
+                        hover:bg-[#8f0e1f]
+                        p-2 rounded-lg
+                        text-white
+                        transition duration-200
+                    "
+                >
+                    <AiFillDelete size={14} />
+                </button>
+
+            </div>
         </div>
     );
 }
